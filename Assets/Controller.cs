713dc55a3d7809eb99832d960345ec5a403dbb95
int <3382634691@qq.com>
2022-10-25 01:03:02 +0800

    人物操控增加

---
+++ Assets/Controller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class Controller : MonoBehaviour
+{
+    public GameObject SeleObj;
+    public GameObject ControlUI;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(myRay.origin.x, myRay.origin.y), Vector2.down);
+
+
+
+            if (SeleObj != null)
+            {
+                Vector3 v3 = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+                SeleObj.gameObject.transform.DOMove(v3, 0.5f);
+                SeleObj.gameObject.GetComponent<PlayerItem>().CancelSelect();
+                SeleObj = null;
+                ControlUI.gameObject.SetActive(false);
+                return;
+            }
+
+            if (hit.collider)
+            {
+                //do something
+                print(hit.collider.transform.name);
+
+                hit.collider.gameObject.GetComponent<PlayerItem>().SetSelect();
+                SeleObj = hit.collider.gameObject;
+                ControlUI.gameObject.SetActive(true);
+            }
+
+           
+
+
+
+        }
+    }
+}
+
--- Assets/PlayerItem.cs
+++ Assets/PlayerItem.cs
@@ -3,30 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
-public class PlayerItem : MonoBehaviour, IPointerDownHandler
+public class PlayerItem : MonoBehaviour
 {
     public Animator animator;
-    //当鼠标点击，即鼠标按下与松开均在该物体上时，触发以下函数
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        
-        //你要触发的代码
-    }
-
-    //当检测到鼠标在该物体上有“按下”操作时，触发以下函数
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        //你要触发的代码
-
-        print("点击该物体了");
-        animator.SetBool("select", true);
-    }
+    
 
   
 
     void Start()
     {
         animator = GetComponent<Animator>();
+    }
+
+
+    public void SetSelect() {
+        animator.SetBool("select", true);
+    }
+
+    public void CancelSelect()
+    {
+        animator.SetBool("select", false);
     }
 
     // Update is called once per frame
