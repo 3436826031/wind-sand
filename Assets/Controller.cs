e2b217c4ce4041b9c77b23deaa79c4419d7cb3fa
3436826031 <3436826031@qq.com>
2022-10-25 21:58:24 +0800

    更新ui  和  表情

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 using DG.Tweening;
 public class Controller : MonoBehaviour
 {
-    public GameObject SeleObj;
+    public GameObject seleObj;
     public GameObject ControlUI;
+    public GameObject controlUInew;
+
+
+    public static Controller instance;
+
     // Use this for initialization
     void Start()
     {
-
+        instance = this;
     }
 
     // Update is called once per frame
@@ -17,37 +22,70 @@ public class Controller : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
+
+          
+
             Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit2D hit = Physics2D.Raycast(new Vector2(myRay.origin.x, myRay.origin.y), Vector2.down);
 
 
+        
 
-            if (SeleObj != null)
+            if (seleObj != null)
             {
                 Vector3 v3 = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
-                SeleObj.gameObject.transform.DOMove(v3, 0.5f);
-                SeleObj.gameObject.GetComponent<PlayerItem>().CancelSelect();
-                SeleObj = null;
+        
+
+                seleObj.gameObject.transform.DOMove(v3, 0.5f);
+                seleObj.gameObject.GetComponent<PlayerItem>().CancelSelect();
+                seleObj = null;
                 ControlUI.gameObject.SetActive(false);
                 return;
             }
 
             if (hit.collider)
             {
+             
                 //do something
                 print(hit.collider.transform.name);
 
                 hit.collider.gameObject.GetComponent<PlayerItem>().SetSelect();
-                SeleObj = hit.collider.gameObject;
+                seleObj = hit.collider.gameObject;
+                opneCtrl();
                 ControlUI.gameObject.SetActive(true);
             }
-
-           
-
 
 
         }
     }
+
+
+
+
+    public void  opneCtrl() {
+        //世界坐标转换为屏幕坐标
+
+
+        Vector3 vector31 = Camera.main.WorldToViewportPoint(seleObj.transform.position);
+
+
+        Vector3 vector32 = seleObj.transform.TransformPoint(seleObj.transform.position);
+
+        Vector3 vector3 = Camera.main.WorldToScreenPoint(seleObj.transform.position);
+     //   controlUInew.transform.position = vector3;
+
+      
+        print("坐标："+vector3+"坐标2："+ vector31);
+
+
+        controlUInew.GetComponent<Animator>().SetBool("open",true);
+
+    }
+
+
+
+
 }
 
---
+++ Assets/Script/Mood.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mood : MonoBehaviour
+{
+
+
+
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+
+
+    
+    }
+
+
+    public bool isUp=true;
+    public bool isDown=false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (isUp) {
+            transform.position += new Vector3(0, 1, 0) * Time.deltaTime * 1.2f;
+            Invoke("endUp", 0.5f);
+        }
+
+
+
+        if (isDown) {
+            transform.position -= new Vector3(0, 1, 0) * Time.deltaTime * 1.2f;
+            Invoke("endDown", 0.5f);
+
+        }
+
+       
+
+
+    }
+
+
+
+
+    void endUp() {
+        isUp = false;
+        Invoke("startDown", 1f);
+    }
+
+
+
+    void startDown() {
+        isDown = true;
+    
+    }
+
+    void endDown() {
+        isDown = false;
+    
+    }
+
+
+
+
+
+}
