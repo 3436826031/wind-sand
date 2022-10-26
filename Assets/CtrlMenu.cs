f83f21f1e54e468017761a55efccc7c34f136c5c
int <3382634691@qq.com>
2022-10-27 01:20:03 +0800

    整顿游戏整体流程，体力机制，还有操作流程

---
+++ Assets/CtrlMenu.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlMenu : MonoBehaviour
+{
+    public GameObject Menu1;
+    public GameObject Menu2;
+}
--- Assets/PlayerItem.cs
+++ Assets/PlayerItem.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using UnityEngine.UI;
 public class PlayerItem : MonoBehaviour
 {
     public Animator animator;
-    
+    public int id;
+    public Image[] ziyuan; //携带的资源
+    private SpriteRenderer spriteRenderer;
+    public bool yijingcaozuo = false;
+    public Color color;
+    public int Tili = 3;
     void Start()
     {
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    public void SetYijingcaozuo() {
+        yijingcaozuo = true;
+        spriteRenderer.color = color;
+    }
+    public void CancelYijingccaozuo() {
+        yijingcaozuo = false;
+        spriteRenderer.color = Color.white;
+
+    }
 
     public void SetSelect() {
         animator.SetBool("select", true);
@@ -32,13 +48,12 @@ public class PlayerItem : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        
-
+      
         if (collision.tag.Equals("plant")) {
             //出现采集指示灯
-            print("可以采集");
+            
             Controller.instance.type = 1;
-            Controller.instance.upControllerUI( "采集");
+            //Controller.instance.upControllerUI( "采集");
             Controller.instance.nowItem = collision.gameObject;
         
         }
@@ -47,12 +62,15 @@ public class PlayerItem : MonoBehaviour
         if (collision.tag.Equals("water")) {
 
             Controller.instance.type = 2;
-            Controller.instance.upControllerUI("采水");
+            //Controller.instance.upControllerUI("采水");
             Controller.instance.nowItem = collision.gameObject;
         }
 
 
     }
+
+
+    
 
 
 }
